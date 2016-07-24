3c0ca7b4f0e5281b5db4750e2ebe224e6e069a07
LeandroAlves1985 <leandroalves22@gmail.com>
2016-07-24 15:07:31 -0300

    Update Teste.java

--- src/config/Teste.java
+++ src/config/Teste.java
@@ -2,25 +2,6 @@ package config;
 
 public class Teste {
 	
-	private Integer id;
-	
-	private String nome;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 	
 	
 
